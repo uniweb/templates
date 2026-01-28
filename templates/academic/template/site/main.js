153184a615e4a71127adebe66da6359a2dcd0ba9
Diego Macrini <macrini@proximify.ca>
2026-01-27 20:25:21 -0500

    fix: use static CSS import to prevent FOUC
    Change from dynamic import (styles: import('#foundation/styles'))
    to static import (import '#foundation/styles') so Vite injects
    a <link> tag into the HTML. This prevents flash of unstyled
    content on initial page load.

--- templates/academic/template/site/main.js
+++ templates/academic/template/site/main.js
@@ -1,6 +1,8 @@
+// Static CSS import - Vite will inject <link> tag into HTML
+import '#foundation/styles'
+
 import { start } from '@uniweb/runtime'
 
 start({
-  foundation: import('#foundation'),
-  styles: import('#foundation/styles')
+  foundation: import('#foundation')
 })
