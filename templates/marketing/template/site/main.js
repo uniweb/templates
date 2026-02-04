b142335fa3e1e6dc369fb45e64f22e2bbefb17e0
Diego Macrini <macrini@proximify.ca>
2026-02-04 00:37:00 -0500

    refactor: simplify official template main.js to single start() call

--- templates/marketing/template/site/main.js
+++ templates/marketing/template/site/main.js
@@ -1,14 +1,7 @@
-import { start, initRuntime } from '@uniweb/runtime'
+import { start } from '@uniweb/runtime'
 
-// __FOUNDATION_CONFIG__ is replaced at build time by the site's Vite config.
-// It tells us whether the foundation is bundled or loaded from a URL at runtime.
-const config = __FOUNDATION_CONFIG__
-
-if (config.mode === 'runtime') {
-  // Runtime mode: foundation loaded from URL via dynamic import()
-  initRuntime({ url: config.url, cssUrl: config.cssUrl })
-} else {
-  // Bundled mode: foundation imported at build time
-  await import('#foundation/styles')
-  start({ foundation: import('#foundation') })
-}
+start({
+  config: __FOUNDATION_CONFIG__,
+  styles: import('#foundation/styles'),
+  foundation: import('#foundation')
+})
